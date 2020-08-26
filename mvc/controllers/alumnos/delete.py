30791284c03505b4fd97010fc0e2a6bd4e36f013
janeaf <51490875+janeaf@users.noreply.github.com>
2020-08-26 03:38:18 -0500

    Update delete.py

--- mvc/controllers/alumnos/delete.py
+++ mvc/controllers/alumnos/delete.py
@@ -9,3 +9,14 @@ class Delete():
             return render.delete() # renderizando formulario.html
         except Exception as e:
             return "Error " + str(e.args)
+        
+        
+    def POST(self, Matricula):
+        try:
+            form = web.input()
+            Matricula = form['Matricula']
+            result = model_alumnos.delete(Matricula)
+            web.seeother('/list')
+        except Exception as e:
+            print(e)
+            return "Error"
