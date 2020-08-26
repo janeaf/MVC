28bc2269d2e518f3da4eeb035994ec6f6d674e1c
janeaf <51490875+janeaf@users.noreply.github.com>
2020-08-26 03:34:25 -0500

    Update update.py

--- mvc/controllers/alumnos/update.py
+++ mvc/controllers/alumnos/update.py
@@ -6,6 +6,25 @@ class Update():
 
     def GET(self):
         try:
-            return render.update() # renderizando formulario.html
+            
+         result = model_alumnos.view(Matricula)[0]
+            #print(result)
+            return render.update(result) 
+        except Exception as e:
+            return "Error " + str(e.args)
+
+    def POST(self, Matricula):
+        try:
+            form = web.input()
+            matricula = form.Matricula
+            nombre = form.Nombre
+            apellido_paterno = form.lastname
+            apellido_materno = form.lastname2
+            edad = form.Edad
+            fecha_nacimiento = form.Fecha
+            sexo = form.Sexo
+            estado_civil = form.Estado
+            result = model_alumnos.update(Nombre, lastname, lastname, Matricula, Edad, Fecha, Sexo, Estado)
+            web.seeother('/list')
         except Exception as e:
             return "Error " + str(e.args)
