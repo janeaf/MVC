e1caeed6297df78bde1a00d7ab9bdb7eef856ae9
janeaf <51490875+janeaf@users.noreply.github.com>
2020-08-28 15:14:16 -0500

    Update insert.py

--- mvc/controllers/alumnos/insert.py
+++ mvc/controllers/alumnos/insert.py
@@ -1,23 +1,20 @@
 import web
-
 import mvc.models.alumnos as alumnos
-
 model_alumnos = alumnos.Alumnos()
-
 render = web.template.render("mvc/views/alumnos/", base="template")
 
 class Insert():
 
     def GET(self):
         try:
-            return render.insert() # renderizando formulario.html
+            return render.insert() 
         except Exception as e:
             return "Error " + str(e.args)
 
     def POST(self):
         try:
             form = web.input()
-            firstname = form.firstname
+            firstname = form.Nombre
             lastname = form.lastname
             lastname2 = form.lastname2
             Matricula = form.Matricula
@@ -25,7 +22,7 @@ class Insert():
             Fecha = form.Fecha
             Sexo = form.Sexo
             Estado = form.Estado
-            model_alumnos.insert(firstname, lastname, lastname2, Matricula, Edad, Fecha, Sexo, Estado)
+            model_alumnos.insert(Nombre, lastname, lastname2, Matricula, Edad, Fecha, Sexo, Estado)
             web.seeother('/list')
         except Exception as e:
             print(e)
